473db78675409dc2532aa98b90dea6f02afa49ec
Fatemeh Rizi <64229510+aws-rizi@users.noreply.github.com>
2020-07-14 09:38:15 -0700

    Version 1.0.0 of the Large payload offloading common library for AWS (#1)
    * Version 1.0.0 of the Large payload offloading common library for AWS

---
+++ src/main/java/com/amazonaws/largepayloadoffloading/CountingOutputStream.java
@@ -0,0 +1,32 @@
+package com.amazonaws.largepayloadoffloading;
+
+import java.io.OutputStream;
+
+/**
+ * This class is used for checking the size of a string without copying the
+ * whole string into memory and converting it to bytes array. Compared to
+ * String.getBytes().length, it is more efficient and reliable for large
+ * strings.
+ */
+class CountingOutputStream extends OutputStream {
+    private long totalSize;
+
+    @Override
+    public void write(int b) {
+        ++totalSize;
+    }
+
+    @Override
+    public void write(byte[] b) {
+        totalSize += b.length;
+    }
+
+    @Override
+    public void write(byte[] b, int offset, int len) {
+        totalSize += len;
+    }
+
+    public long getTotalSize() {
+        return totalSize;
+    }
+}
